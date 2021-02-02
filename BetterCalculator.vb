bacd3f3e23131f0147b8a6ac484d147d7cbf205f
FroYoMan <34078793+FroYoMan@users.noreply.github.com>
2021-02-01 23:04:38 -0700

    Add project files.

---
+++ BetterCalculator.vb
@@ -0,0 +1,87 @@
+﻿'Sean Gingerich
+'RCET0265
+'Fall 2020
+'Say My Name Again
+'https://github.com/gingsean5/BetterCalculator
+Module BetterCalculator
+
+    Sub Main()
+        Dim userInput As String
+        Dim userDecision As String
+
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+
+
+        Dim problem As Boolean
+
+
+
+        While userInput <> "Q" And userInput <> "q"
+            Console.WriteLine("Please enter two numbers. Enter ""Q"" at any time to quit.")
+            problem = True
+            While problem = True And userInput <> "Q" And userInput <> "q"
+                Console.Write("Choose a Number: ")
+                Try
+                    userInput = Console.ReadLine()
+                    firstNumber = CInt(userInput)
+                    problem = False
+                Catch e As Exception
+                    Console.WriteLine($"You entered {userInput}, please enter a whole number.")
+                    problem = True
+                End Try
+                If problem = False Then
+                    Console.WriteLine($"You entered {userInput}")
+                End If
+            End While
+
+            problem = True
+
+            While problem = True And userInput <> "Q" And userInput <> "q"
+                Console.Write("Choose a Number: ")
+                Try
+                    userInput = Console.ReadLine()
+                    secondNumber = CInt(userInput)
+                    problem = False
+                Catch e As Exception
+                    Console.WriteLine($"You entered {userInput}, please enter a whole number.")
+                    problem = True
+                End Try
+                If problem = False Then
+                    Console.WriteLine($"You entered {userInput}")
+                End If
+            End While
+            problem = True
+            While problem = True And userDecision <> "Q" And userInput <> "q"
+                Console.WriteLine("Choose one of the following options:")
+                Console.WriteLine("1. Add")
+                Console.WriteLine("2. Subtract")
+                Console.WriteLine("3. Multiply")
+                Console.WriteLine("4. Divide")
+                userDecision = Console.ReadLine()
+
+                Select Case userDecision
+                    Case = "1"
+                        Console.WriteLine($"You entered {userDecision}")
+                        Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}")
+                        problem = False
+                    Case = "2"
+                        Console.WriteLine($"You entered {userDecision}")
+                        Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}")
+                        problem = False
+                    Case = "3"
+                        Console.WriteLine($"You entered {userDecision}")
+                        Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}")
+                        problem = False
+                    Case = "4"
+                        Console.WriteLine($"You entered {userDecision}")
+                        Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}")
+                        problem = False
+                End Select
+            End While
+
+        End While
+    End Sub
+
+
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SimpleCalculatorAgain")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("SimpleCalculatorAgain")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("167dddc8-a5f0-4c05-93d2-95c5b487169a")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
